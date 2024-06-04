fdb4d65952cb406b9ee02b5b067e13c4c71aa9a5
Alejandro Carrillo <carril52@msu.edu>
2024-06-04 14:56:51 -0400

    Added new files, created quarto doc for cleaning_Python_files

--- Study 1/scripts/cleaning.py
+++ Study 1/scripts/cleaning.py
@@ -1,5 +1,19 @@
 # Creating the cleaning file based on my original R script 
+# Libraries 
+import pandas as pd
+
+# Testing the function
+# Import the csv file and call it df
+df = pd.read_csv('Study 1/data/10322_VAST1_2023-11-01_14h13.15.208.csv')
+# View the excel_file's sheet names
+print(df)
 
 #### SECTION 1: Long Form ####
 
 # Start by writing the function to do prepare the data
+def longformVASTcleaning(data_original):
+    # copy the original data to a new dataframe
+    df = data_original
+    # remove practice trials 
+
+    pass 
